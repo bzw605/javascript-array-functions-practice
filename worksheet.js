c73ec3327af4cf1b086f0abc2090fbfbe5485d70
bzw605 <bzw605@gmail.com>
2022-07-23 17:27:01 -0400

    asdsad

--- worksheet.js
+++ worksheet.js
@@ -1,36 +1,37 @@
 const characters = [
-    {
-        name: 'Luke Skywalker',
-        height: 172,
-        mass: 77,
-        eye_color: 'blue',
-        gender: 'male',
-    },
-    {
-        name: 'Darth Vader',
-        height: 202,
-        mass: 136,
-        eye_color: 'yellow',
-        gender: 'male',
-    },
-    {
-        name: 'Leia Organa',
-        height: 150,
-        mass: 49,
-        eye_color: 'brown',
-        gender: 'female',
-    },
-    {
-        name: 'Anakin Skywalker',
-        height: 188,
-        mass: 84,
-        eye_color: 'blue',
-        gender: 'male',
-    },
+  {
+    name: "Luke Skywalker",
+    height: 172,
+    mass: 77,
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: 202,
+    mass: 136,
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: 150,
+    mass: 49,
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: 188,
+    mass: 84,
+    eye_color: "blue",
+    gender: "male",
+  },
 ];
 
 //***MAP***
 //1. Get array of all names
+
 //2. Get array of all heights
 //3. Get array of objects with just name and height properties
 //4. Get array of all first names
@@ -43,6 +44,11 @@ const characters = [
 
 //***FILTER***
 //1. Get characters with mass greater than 100
+const greater100 = characters.filter((character) => {
+  return character.mass > 100;
+});
+console.log(greater100);
+
 //2. Get characters with height less than 200
 //3. Get all male characters
 //4. Get all female characters
